461159552827b819af9a6b2033593a81099654c3
alex-usa <gerasimovusa@gmail.com>
2024-11-02 14:16:01 +0500

    29/10

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Alex'
+print('Name: ' + name)
+age = 43
+print('Age: ' + str(age))
+new_age = age + 1
+print('New age: ' + str(new_age))
+is_student = True
+print('Is Student: ' + str(is_student))
