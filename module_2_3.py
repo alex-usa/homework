0fd31d323557d0a0349cf8c83183fa956340cce9
alex-usa <gerasimovusa@gmail.com>
2024-11-14 13:01:45 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,20 @@
+print('Первый вариант решения:')
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    if my_list[index] < 0:
+        break
+    elif my_list[index] > 0:
+        print(my_list[index])
+    index += 1
+print('Второй вариант решения:')
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    if my_list[index] > 0:
+        print(my_list[index])
+        index += 1
+    elif my_list[index] == 0:
+        index += 1
+    elif my_list[index] < 0:
+        break
